17d56e6389c892803747ae97f791ae29954b3089
John M. Daniel <imjohnmdaniel@ce-v.com>
2021-10-17 21:43:46 -0400

    setup of basic commands

---
+++ src/commands/toolbox/project/stage/testing.ts
@@ -0,0 +1,43 @@
+import { flags, SfdxCommand } from '@salesforce/command';
+import { Messages } from '@salesforce/core';
+import { AnyJson } from '@salesforce/ts-types';
+
+// Initialize Messages with the current plugin directory
+Messages.importMessagesDirectory(__dirname);
+
+// Load the specific messages for this file. Messages from @salesforce/command, @salesforce/core,
+// or any library that is using the messages framework can also be loaded this way.
+const messages = Messages.loadMessages('@dx-cli-toolbox/sfdx-toolbox-project-utils', 'toolbox-project-stage-testing');
+
+export default class Testing extends SfdxCommand {
+
+    public static description = messages.getMessage('commandDescription');
+
+    public static examples = [messages.getMessage('examplesDescription')];
+
+    //   public static args = [{name: 'file'}];
+
+    protected static flagsConfig = {
+        // prompt: flags.boolean({ char: 'p', default: false, required: false, description: messages.getMessage('flagPromptDescription') })
+    };
+
+    // Comment this out if your command does not require an org username
+    protected static requiresUsername = true;
+
+    // Comment this out if your command does not require a hub org username
+    protected static requiresDevhubUsername = false;
+
+    // If true, then the command supported the parameter of specifying the hub org username
+    protected static supportsDevhubUsername = false;
+
+    // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
+    protected static requiresProject = true;
+
+    public async run(): Promise<AnyJson> {
+        this.ux.log('TODO Need to implement toolbox:project:stage:testing command');
+
+        // Return an object to be displayed with --json
+        // return { orgId: this.org.getOrgId(), outputString };
+        return;
+    }
+}
